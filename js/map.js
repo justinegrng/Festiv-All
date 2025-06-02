a0b79c0ff8f6ca484b5be2199b5c55723aab9f18
Marin Tusseau <marin.tusseau@efrei.net>
2025-06-02 20:17:37 +0200

    version quasi-final

--- js/map.js
+++ js/map.js
@@ -5,35 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([46.155286877974554, -1.157270462871814]).addTo(map)
-        .bindPopup('Francofolies, La Rochelle')
+    const festivals = {
+        "Francofolies, La Rochelle": [46.155286877974554, -1.157270462871814],
+        "Garorock, Marmande": [44.489962811045956, 0.16464310516195382],
+        "Burdigala Fest, Bordeaux": [44.83879938988657, -0.5911211568125361],
+        "Initial, Bordeaux": [44.89382970475388, -0.5761740647613016],
+        "ODP, Talence": [44.80809096000953, -0.5898247034198687],
+        "Balterno, Bègles": [44.79525459548204, -0.5600134782726451],
+        "SunSka, Vertheuil": [45.24430221491649, -0.8875177511068462],
+        "We Love Green, Paris": [48.82833970667415, 2.433107925643727],
+        "Les Vieilles Charrues, Carhaix-Plouguer": [48.27104108005381, -3.5602270941012213],
+        "Tomorrowland, Boom": [51.091738788113645, 4.385633381593768]
+    };
 
-    L.marker([44.489962811045956, 0.16464310516195382]).addTo(map)
-        .bindPopup('Garorock, Marmande')
-
-    L.marker([44.83879938988657, -0.5911211568125361]).addTo(map)
-        .bindPopup('Burdigala Fest, Bordeaux')
-
-    L.marker([44.89382970475388, -0.5761740647613016]).addTo(map)
-        .bindPopup('Initial, Bordeaux')
-
-    L.marker([44.80809096000953, -0.5898247034198687]).addTo(map)
-        .bindPopup('ODP, Talence')
-
-    L.marker([44.79525459548204, -0.5600134782726451]).addTo(map)
-        .bindPopup('Balterno, Bègles')
-
-    L.marker([45.24430221491649, -0.8875177511068462]).addTo(map)
-        .bindPopup('SunSka, Vertheuil')
-
-    L.marker([48.82833970667415, 2.433107925643727]).addTo(map)
-        .bindPopup('We Love Green, Paris')
-
-    L.marker([48.27104108005381, -3.5602270941012213]).addTo(map)
-        .bindPopup('Les Vieilles Charrues, Carhaix-Plouguer')
-
-    L.marker([51.091738788113645, 4.385633381593768]).addTo(map)
-        .bindPopup('Tomorrowland, Boom')
+    for (const [name, coords] of Object.entries(festivals)) {
+        L.marker(coords).addTo(map).bindPopup(name);
+    }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
